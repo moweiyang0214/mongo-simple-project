c15514456553baa408ac3ea4980db850b1103b7c
wanglei <wanglei@r.recruit.co.jp>
2018-12-03 20:21:44 +0900

    add schema

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/playground')
   .then(()=> console.log('connected to MongoDb...'))
   .catch(err => console.error('could not connect to MongoDb', err))
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [ String ],
+  data: { type: Date, default: Date.now },
+  isPublished: Boolean
+})
+
+// Classes, objects
+const Course = mongoose.model('Course', courseSchema)
+const course = new Course({
+  name: 'Node.js Course',
+  author: 'lei',
+  tags: ['node', 'backend'],
+  isPublished: true
+})
