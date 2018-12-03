a4b1e6ff17cdce841657e2f6a55f5a828719710e
wanglei <wanglei@r.recruit.co.jp>
2018-12-03 21:36:54 +0900

    fix exercise with query for searching documents

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // ssh-keygen -t rsa
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/mongo-exercises')
   .then(()=> console.log('connected to MongoDb...'))
   .catch(err => console.error('could not connect to MongoDb', err))
 
@@ -9,36 +9,42 @@ const courseSchema = new mongoose.Schema({
   author: String,
   tags: [ String ],
   data: { type: Date, default: Date.now },
-  isPublished: Boolean
+  isPublished: Boolean,
+  price: Number
 })
 
 // Classes, objects
 const Course = mongoose.model('Course', courseSchema)
 
-async function  createCourse() {
-  const course = new Course({
-    name: 'Angular Course',
-    author: 'lei',
-    tags: ['angular', 'frontend'],
-    isPublished: true
-  })
-  const result = await course.save()
-  console.log(result)
-}
+// async function  createCourse() {
+//   const course = new Course({
+//     name: 'Angular Course',
+//     author: 'lei',
+//     tags: ['angular', 'frontend'],
+//     isPublished: true
+//   })
+//   const result = await course.save()
+//   console.log(result)
+// }
 
 async function getCourses() {
+  const course = await Course
+    .find({ isPublished: true, tags: 'backend'})
+    .sort({price: 1})
+    .select({name: 1, author: 1, price: 1})
+  console.log(course)
   // /api/course?pageNumber=2&pageSize=10
 
-  const pageNumber = 2
-  const pageSize = 10
+  // const pageNumber = 2
+  // const pageSize = 10
 
-  const courses = await Course
-  .find()
-  .skip((pageNumber - 1)* pageSize)
-  .limit(pageSize)
-  .sort({name: 1})
-  .select({ name: 1, tags: 1})
-  console.log(courses)
+  // const courses = await Course
+  // .find()
+  // .skip((pageNumber - 1)* pageSize)
+  // .limit(pageSize)
+  // .sort({name: 1})
+  // .select({ name: 1, tags: 1})
+  // console.log(courses)
   // comparasion: eq(equal) 
   // ne: not equal 
   // gt: greater than 
